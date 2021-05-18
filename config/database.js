b9069fd7d5d3a04948fae64f50625c0458f4cd64
mmeguizo <mark.meguizo@gmail.com>
2021-05-19 01:50:00 +0800

    first envs

--- config/database.js
+++ config/database.js
@@ -3,13 +3,14 @@ const key = require('dotenv').config()
 const crypto = require('crypto');
 
 
-const hash = crypto.createHmac('sha256', process.env.SECRET).update('meguizo_starter').digest('hex');
+const hash = crypto.createHmac('sha256', 'MEGUIZO').update('meguizo_starter').digest('hex');
 
 module.exports = {
     // using localhost
    //uri: 'mongodb://localhost:27017/scamwatch',
+   uri: 'mongodb+srv://meguizo:qAFOCPuo8vclyKjT@cluster0.bxhyy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
     // if deployed online
-    uri: process.env.DB_HOST,
+    //uri: process.env.DB_HOST,
     secret: hash,
     options: {
         useUnifiedTopology: true,
